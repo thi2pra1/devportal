fc154bb4a6fbc31808e0bdf7e9a1967c27b493d3
Thiago Carneiro <thijack2000@gmail.com>
2022-05-09 14:58:24 -0300

    corrigindo problema de autenticacao

--- packages/backend/src/plugins/auth.ts
+++ packages/backend/src/plugins/auth.ts
@@ -1,4 +1,4 @@
-import { createRouter, createGithubProvider } from '@backstage/plugin-auth-backend';
+import { createRouter } from '@backstage/plugin-auth-backend';
 import { Router } from 'express';
 import { PluginEnvironment } from '../types';
 
@@ -10,36 +10,6 @@ export default async function createPlugin(
     config: env.config,
     database: env.database,
     discovery: env.discovery,
-    tokenManager: env.tokenManager,
-    providerFactories: {
-      github: createGithubProvider({
-        authHandler: async ({
-          fullProfile: {
-            displayName,
-            photos,
-            username,
-          },
-        }, ctx) => {
-          if (username === undefined) {
-            throw new Error();
-          }
-          await ctx.catalogIdentityClient.findUser({
-            annotations: {
-              'github.com/user-login': username,
-            },
-          });
-          let picture:string|undefined;
-          if (photos !== undefined && photos[0] !== undefined) {
-            picture = photos[0].value;
-          }
-          return {
-            profile: {
-              picture,
-              displayName,
-            }
-          };
-        },
-      }),
-    },
+    tokenManager: env.tokenManager
   });
 }
